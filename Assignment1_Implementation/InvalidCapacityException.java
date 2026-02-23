af1074fa8b812ba5fc61cd5d21100a354417cf3a
phuay1in <phuaylin@montgomerycollege.edu>
2026-02-22 21:57:53 -0500

    Delete Assignment1_Implementation/InvalidCapacityException.java

--- Assignment1_Implementation/InvalidCapacityException.java
+++
@@ -1,8 +0,0 @@
-public class InvalidCapacityException extends Exception {
-    public InvalidCapacityException(String message) {
-        super(message);
-    }
-    public InvalidCapacityException() {
-    	super("Invalid size");
-    }
-}
